8e7f9abf295d98e20a81efb1283340270b6612da
Md Shihab Sikder <86490994+mdshihab56@users.noreply.github.com>
2026-08-14 22:44:21 +0600

    Add files via upload

---
+++ arithmatic_operation.java
@@ -0,0 +1,37 @@
+
+
+public class arithmatic_operation {
+    public static void main(String[] args) {
+        
+        //Arithmatic operations
+
+        //Assigned values to variable x and y
+        int x=10;
+        int y=2;
+
+        //declaring resultant variables
+        int z;
+        int q;
+        int a;
+        int b;
+        int c;
+
+        //arithmatic operations
+        z=x+y;
+        q=x-y;
+        a=x*y;
+        b=x/y;
+        c=x%y;
+
+
+        //ptinting output
+        System.err.println("Addiotion value: "+z);
+        System.err.println("Substraction value: "+q);
+        System.err.println("MUltiplication value: "+a);
+        System.err.println("Divided value: "+b);
+        System.err.println("Modulas value: "+c);
+    
+
+    }
+    
+}
