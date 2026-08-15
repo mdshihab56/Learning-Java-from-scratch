edf5adf536df500d961c96e8fad7b40fab434d0f
Md Shihab Sikder <86490994+mdshihab56@users.noreply.github.com>
2026-08-15 15:45:26 +0600

    Add files via upload

---
+++ augmented_assignment_operators.java
@@ -0,0 +1,24 @@
+public class augmented_assignment_operators {
+    public static void main(String[] args) {
+        
+        //Augmented Assignment Operators
+
+        //assigned values to variable x and y
+        int x= 10;
+        int y=2;
+
+        //augment variable resultant
+        // remove "/" to use the specific operator
+         x += y;
+        // x-= y;
+        // x*= y;
+        // x/= y;
+        // x%= y;
+
+    
+        //ptinting output
+        System.err.println("Augmented value: "+x);
+      
+    }
+    
+}
