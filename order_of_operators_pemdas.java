22d607334f6e1dd6b954c32e3f68e9e42f0c3807
Md Shihab Sikder <86490994+mdshihab56@users.noreply.github.com>
2026-08-17 00:33:11 +0600

    Add files via upload

---
+++ order_of_operators_pemdas.java
@@ -0,0 +1,16 @@
+public class order_of_operators_pemdas {
+    public static void main(String[] args) {
+        
+        //order of operations pemdas
+
+        //assigning values for result
+        double result=3+4*(7-5)/2.0;
+
+
+        //printing resultant output
+        System.out.println("\nResult: "+result);
+        
+
+    }
+    
+}
