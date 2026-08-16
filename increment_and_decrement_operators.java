3b5e97c1056eab56589998528980aa70e1e6cfb3
Md Shihab Sikder <86490994+mdshihab56@users.noreply.github.com>
2026-08-16 17:26:58 +0600

    Add files via upload

---
+++ increment_and_decrement_operators.java
@@ -0,0 +1,26 @@
+public class increment_and_decrement_operators {
+    public static void main(String[] args) {
+        
+        //increment and decrement operrators
+
+        //assigned variable values for x and y
+        int x=1;
+        int y=2;
+
+        //incrementing x
+        x=x+1;
+        //x+=1
+        //x++
+
+        //incrementing y
+        y=y-2;
+        //y-=2
+        //y--
+
+
+        //prints output
+        System.out.println("\nIncremented value: "+x);
+        System.out.println("Decremented value: "+y);
+    }
+    
+}
